e6f9f4f6819e316efb8b4042e64ce7318f97e599
Tomas Prosek <tommy.prosek@gmail.com>
2018-05-13 21:22:59 +0200

    Initial commit

---
+++ taskr/lib/db.php
@@ -0,0 +1,10 @@
+<?php
+$dbServername = "127.0.0.1";
+$dbUsername = "proseto17";
+$dbPassword = "bOGust56";
+$dbName = "proseto17";
+
+$conn = mysql_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+    global $conn;
+    
+?>
