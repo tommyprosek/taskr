e2c1e224f0dcf9b66e06a9571d0383b87fe64877
Tomas Prosek <tommy.prosek@gmail.com>
2018-06-02 17:48:09 +0200

    Refactor based on testing

--- taskr/lib/db.php
+++ taskr/lib/db.php
@@ -1,27 +1,41 @@
 <?php
 include "password_hash.php";
-//$dbServer = "127.0.0.1";
-////$dbUsername = "proseto17";
-////$dbPassword = "bOGust56";
-//$dbUsername = "root";
-//$dbPassword = "";
-//$dbName = "proseto17";
+// PRODUCTION
+//$host = '127.0.0.1';
+//$db   = 'proseto17';
+//$user = 'proseto17';
+//$pass = 'bOGust56';
+//$charset = 'utf8';
 
+// DEVELOPMENT
+//$host = '127.0.0.1';
+//$db   = 'proseto17';
+//$user = 'root';
+//$pass = '';
+//$charset = 'utf8';
 
-function connect (){
+function connect()
+{
     $host = '127.0.0.1';
-    $db   = 'proseto17';
+    $db = 'proseto17';
     $user = 'root';
     $pass = '';
     $charset = 'utf8';
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
 // TODO tommy - vygoogle co jsou tyhle parametry zač a napsat je sem do komentare
-    $opt = [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    $opt = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false,
-    ];
+        PDO::ATTR_EMULATE_PREPARES => false
+    );
+
+//    $opt = [
+//        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+//        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+//        PDO::ATTR_EMULATE_PREPARES => false,
+//    ];
     try {
         return new PDO($dsn, $user, $pass, $opt);
     } catch (PDOException $e) {
@@ -30,7 +44,8 @@ function connect (){
     }
 }
 
-function findUserByEmail($email) {
+function findUserByEmail($email)
+{
     $connection = connect();
     $sql = "SELECT * FROM users where email=:email";
     $statement = $connection->prepare($sql);
@@ -38,7 +53,8 @@ function findUserByEmail($email) {
     return $statement;
 }
 
-function findTasksByUserId ($user_id) {
+function findTasksByUserId($user_id)
+{
     $connection = connect();
     $sql = "select t.task_id, t.title, t.deadline, c.name as category_name, t.done from tasks t
 left join categories c on t.category_id = c.category_id
@@ -48,16 +64,18 @@ where t.user_id=:user_id";
     return $statement;
 }
 
-function createUser ($first_name, $last_name, $password, $email) {
+function createUser($first_name, $last_name, $email, $password)
+{
     $connection = connect();
-    $sql = "INSERT INTO users (first_name, last_name, password, email) VALUES (:first_name, :last_name, :password, :email)";
+    $sql = "INSERT INTO users (first_name, last_name, password, email) VALUES (:first_name, :last_name, :hash, :email)";
     $statement = $connection->prepare($sql);
     $hash = create_password_hash($password);
     $statement->execute(array($first_name, $last_name, $hash, $email));
     return $statement;
 }
 
-function createTask ($user_id, $title, $deadline, $category_id) {
+function createTask($user_id, $title, $deadline, $category_id)
+{
     $connection = connect();
     $sql = "INSERT INTO tasks (user_id, title, deadline, category_id)
     values (:user_id, :title, :deadline, :category_id )";
@@ -66,7 +84,8 @@ function createTask ($user_id, $title, $deadline, $category_id) {
     return $statement;
 }
 
-function findCategories () {
+function findCategories()
+{
     $connection = connect();
     $sql = "SELECT * from categories";
     $statement = $connection->prepare($sql);
@@ -74,7 +93,8 @@ function findCategories () {
     return $statement;
 }
 
-function updateTaskFinished ($task_id, $user_id) {
+function updateTaskFinished($task_id, $user_id)
+{
     $connection = connect();
     $sql = "UPDATE tasks SET done=true WHERE task_id=:task_id AND user_id=:user_id";
     $statement = $connection->prepare($sql);
@@ -82,7 +102,8 @@ function updateTaskFinished ($task_id, $user_id) {
     return $statement;
 }
 
-function deleteTask ($task_id, $user_id) {
+function deleteTask($task_id, $user_id)
+{
     $connection = connect();
     $sql = "DELETE FROM tasks WHERE task_id=:task_id AND user_id=:user_id";
     $statement = $connection->prepare($sql);
